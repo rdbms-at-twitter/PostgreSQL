d58d52e77c36665490c8b118b3f7d7d1ffa10bbf
sugiyama201909 <shinya@N-D004-Sugiyama.localdomain>
2021-01-06 11:49:01 +0900

    check pg connection with user and ip

---
+++ check_pg_connections.sql
@@ -0,0 +1,1 @@
+select current_user,inet_server_addr( ), inet_server_port( ),usename,client_addr,query_start,state,application_name,left(query,60) from pg_stat_activity where query <> '<insufficient privilege>' limit  10;
