d847fec743f7c8b55c553c5400c387366f6193ca
sugiyama201909 <shinya@N-D004-Sugiyama.localdomain>
2021-06-03 17:21:43 +0900

    check_table_size_in_current_schema

---
+++ check_table_size_in_current_schema.sql
@@ -0,0 +1,10 @@
+SELECT
+pp.relname as "Table",
+pc.relkind AS objecttype,
+pc.reltuples AS "#entries", 
+pg_size_pretty(pg_total_relation_size(pp.relid)) As "Total Size",
+pg_size_pretty(pc.relpages::bigint*8*1024) AS "Table size",
+pg_size_pretty(pg_total_relation_size(pp.relid) - pg_relation_size(pp.relid)) as "External Size(idx)"
+FROM pg_catalog.pg_statio_user_tables pp inner join pg_class pc on pp.relname = pc.relname
+inner join pg_stat_all_tables pat on pat.relname = pc.relname
+ORDER BY pg_total_relation_size(pp.relid) DESC limit 10;
